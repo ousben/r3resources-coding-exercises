8496681950de489efa7c7d20afba41a9f980db06
unknown <benguiratoussama1998@gmail.com>
2023-12-18 13:24:04 +0100

    exercise 12 not finished

---
+++ javascript-exercises/JavaScript-string/exercise-12/script.js
@@ -0,0 +1,11 @@
+function uncamelize(string, separator) {
+  let words = [];
+  for(let i = 0; i < words.length; i++) {
+    let word = "";
+    if(string[i] === string[i].toUpperCase()) {
+
+    } else {
+      
+    }
+  }
+}
