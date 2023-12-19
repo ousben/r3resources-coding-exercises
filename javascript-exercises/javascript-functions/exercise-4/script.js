c287d3aa67b6ab24e8b13263c5d36d1c572089b0
unknown <benguiratoussama1998@gmail.com>
2023-12-19 16:43:48 +0100

    exercise 4

--- javascript-exercises/javascript-functions/exercise-4/script.js
+++ javascript-exercises/javascript-functions/exercise-4/script.js
@@ -1,5 +1,14 @@
-if("v" < "w") {
-  console.log(true);
-} else {
-  console.log(false);
+function alphabeticalOrder(string) {
+  let result = [];
+  for(let k = 0; k < string.length; k++) {
+    result.push(string[k]);
+  }
+  for(let i = 0; i < result.length; i++) {
+    for(let j = 0; j < result.length - i - 1; j++) {
+      if(result[j+1] < result[j]) {
+        [result[j+1], result[j]] = [result[j], result[j+1]];
+      }
+    }
+  }
+  return result;
 }
