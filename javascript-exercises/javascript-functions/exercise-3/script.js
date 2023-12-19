518d267d2aad27c044446ce7aea5eaddb93e83c0
unknown <benguiratoussama1998@gmail.com>
2023-12-19 16:04:47 +0100

    exercise 3

--- javascript-exercises/javascript-functions/exercise-3/script.js
+++ javascript-exercises/javascript-functions/exercise-3/script.js
@@ -1,6 +1,9 @@
 function allCombinations(string) {
-  let result = [];
+  let result = []
   for(let i = 0; i < string.length; i++) {
-    
+    for(let j = i; j < string.length; j++) {
+      result.push(string.slice(i, j+1));
+    }
   }
+  return result;
 }
---
+++ javascript-exercises/javascript-functions/exercise-4/script.js
@@ -0,0 +1,5 @@
+if("v" < "w") {
+  console.log(true);
+} else {
+  console.log(false);
+}
