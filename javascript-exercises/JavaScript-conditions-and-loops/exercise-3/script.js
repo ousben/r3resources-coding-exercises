ec234f6649ecd8df0141c071b7254cf1c0956d6f
unknown <benguiratoussama1998@gmail.com>
2023-12-20 15:02:26 +0100

    exercise 3

--- javascript-exercises/JavaScript-conditions-and-loops/exercise-3/script.js
+++ javascript-exercises/JavaScript-conditions-and-loops/exercise-3/script.js
@@ -1,7 +1,7 @@
 function sortArray(array) {
   for(let i = 0; i < array.length; i++) {
     for(let j = 0; j < array.length - i - 1; j++) {
-      if(array[j] > array[j+1]) {
+      if(array[j] < array[j+1]) {
         [array[j], array[j+1]] = [array[j+1], array[j]]
       }
     }
