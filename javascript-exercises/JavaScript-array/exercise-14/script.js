e2141652fe3ff7b019ee8579ff26ba977bce1925
unknown <benguiratoussama1998@gmail.com>
2023-12-23 13:35:11 +0100

    exercise 14

---
+++ javascript-exercises/JavaScript-array/exercise-14/script.js
@@ -0,0 +1,10 @@
+function removeDuplicate(arr) {
+  let array = arr.map(item => {
+    return item.toLowerCase();
+  })
+  for(let i = 0; i < array.length; i++) {
+    if(array[i]) {}
+  }
+}
+
+console.log(removeDuplicate(["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "]));
