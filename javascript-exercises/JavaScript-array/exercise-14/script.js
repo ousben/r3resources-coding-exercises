eca11459f915790f567206608bef5857de7ad1f4
unknown <benguiratoussama1998@gmail.com>
2024-01-05 19:06:38 +0100

    exercise 14

--- javascript-exercises/JavaScript-array/exercise-14/script.js
+++ javascript-exercises/JavaScript-array/exercise-14/script.js
@@ -1,8 +1,17 @@
 function removeDuplicate(arr) {
   let array = arr.map(item => {
     return item.toLowerCase();
-  })
+  });
+  let toDelete = [];
+  for(let i = 0; i < array.length; i++) {
+    for(let j = i + 1; j <  array.length; j++) {
+      if(array[i] === array[j]) {
+        toDelete.push(j);
+      }
+    }
+  }
+  for(let i = 0; i < toDelete.length; i++) {
+    array.splice(toDelete[i] - i, 1);
+  }
   return array;
 }
-
-console.log(removeDuplicate(["Blue", "Green", "Orange", "Green", "Orange", "Indigo", "Indigo"]));
